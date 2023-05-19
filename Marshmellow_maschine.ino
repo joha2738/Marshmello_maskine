e0ec7438b9cb60d8cded5eca25f9ec56ff0fad06
Johannes van Roest Dahl <65940893+joha2738@users.noreply.github.com>
2023-05-19 21:39:42 +0200

    Add files via upload

--- Marshmellow_maschine.ino
+++ Marshmellow_maschine.ino
@@ -1,5 +1,6 @@
 /*Stepper motor driver uden bibliotek, til Den perfekt Marshmello maskine*/
 #include <Adafruit_NeoPixel.h>
+#include <pins.ino>
 #define PIN 23 //Indbyggede WS2812 LED
 #define NUMPIXELS 1
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
@@ -8,39 +9,43 @@ int buttonPushCounter = 0;  // tæller til antal tastetryk
 int buttonState = 0;        // Nuværende position af tastetryk
 int lastButtonState = 0;    // Forrige position af tastetryk
 
-// Define stepper motor connections and steps per revolution:
-#define dirPin     2           //  m0Pin  m1Pin  m2Pin    Step
-#define stepPin    3           //  L      L      L        Full
-#define sleepPin   4           //  H      L      L        1/2
-#define resetPin   5           //  L      H      L        1/4
-#define m2Pin      9           //  H      H      L        1/8
-#define m1Pin      6           //  L      L      H        1/16
-#define m0Pin      7           //  H      L      H        1/32
-#define enablePin  8           //  
-#define pause      1           //  Disconected = L L L
-#define steps      125         // 125 steps passer med 1/4 step hastiged
-#define buttonPin  17          // Bruges til at aktivere mekanismen
-#define buttonPin2 16          // Ikke i brug endnu men lagt ud i skrueterminal
+
 
 void setup() {
+  // Declare pins as output:
   pinMode(dirPin, OUTPUT);
+  pinMode(dirPin2, OUTPUT);
   pinMode(stepPin, OUTPUT);
+  pinMode(stepPin2, OUTPUT);
   pinMode(sleepPin, OUTPUT);
+  pinMode(sleepPin2, OUTPUT);
   pinMode(resetPin, OUTPUT);
+  pinMode(resetPin2, OUTPUT);
   pinMode(m2Pin, OUTPUT);
+  pinMode(m2Pin2, OUTPUT);
   pinMode(m1Pin, OUTPUT);
+  pinMode(m1Pin2, OUTPUT);
   pinMode(m0Pin, OUTPUT);
+  pinMode(m0Pin2, OUTPUT);
   pinMode(enablePin, OUTPUT);
+  pinMode(enablePin2, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(buttonPin2, INPUT_PULLUP);
+
+  pinMode(sensor, INPUT)
+  pinMode(sensorLED, OUTPUT)
 
   pixels.begin();
   pixels.clear();
   pixels.show();
 
-  // 1/4 steps er meget passende til den roterende skuffe
   digitalWrite(m0Pin, LOW);
   digitalWrite(m1Pin, HIGH);
   digitalWrite(m2Pin, LOW);
+
+  digitalWrite(m0Pin2, LOW);
+  digitalWrite(m1Pin2, HIGH);
+  digitalWrite(m2Pin2, LOW);
 }
 
 void loop() {
@@ -48,10 +53,21 @@ void loop() {
   if (buttonState != lastButtonState) {
     if (buttonState == HIGH) {
       buttonPushCounter++;
-      //Start motor
+      skuffe();
+      rotor();
+    }
+  } else {
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+    pixels.show();
+  }
+  lastButtonState = buttonState;
+}
+
+void skuffe(){
+//Start motor
       digitalWrite(sleepPin, HIGH);
       digitalWrite(resetPin, HIGH);
-      //Sæt retning: skub ud
+      //Sæt retning højreom
       digitalWrite(dirPin, LOW);
       //Tænd LED Grøn
       pixels.setPixelColor(0, pixels.Color(0, 5, 0));
@@ -67,7 +83,7 @@ void loop() {
       delay(200);
 
 
-      //Sæt retning: træk ind
+      //Sæt retning venstreom
       digitalWrite(dirPin, HIGH);
       //Tænd LED Blå
       pixels.setPixelColor(0, pixels.Color(0, 0, 5));
@@ -81,13 +97,32 @@ void loop() {
         delayMicroseconds(500);
       }
 
-      //Sluk motor
+            //Sluk motor
       digitalWrite(sleepPin, LOW);
       digitalWrite(resetPin, LOW);
-    }
-  } else {
-    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+
+}
+
+void rotor(){
+  //Start motor
+    digitalWrite(sleepPin2, HIGH);
+    digitalWrite(resetPin2, HIGH);
+  //Sæt retning højreom
+    digitalWrite(dirPin2, LOW);
+  //Tænd LED Grøn
+    pixels.setPixelColor(0, pixels.Color(0, 5, 5));
     pixels.show();
-  }
-  lastButtonState = buttonState;
+  //Drejer motoren i antallet af steps
+    for (int i = 0; i < steps2; i++) {
+      delay(pause);
+      digitalWrite(stepPin2, HIGH);
+      delayMicroseconds(500);
+      digitalWrite(stepPin2, LOW);
+      delayMicroseconds(500);
+      }
+    delay(200);
+  //Sluk motor
+    digitalWrite(sleepPin2, LOW);
+    digitalWrite(resetPin2, LOW);
+
 }
